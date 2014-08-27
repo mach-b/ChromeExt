49606d7a8905b8a59810cbef55c44b7e24cdde8d
mach-b <markburton@Marks-MacBook-Pro.local>
2014-08-28 11:50:19 +1200

    Update

--- background.js
+++ background.js
@@ -2,24 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// var backgroundWindow = chrome.windows.create(
+//    {url: "background.html?" + tabId, type: "popup", width: 800, height: 600});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.debugger.attach({tabId:tab.id}, version,
       onAttach.bind(null, tab.id));
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+  if (changeInfo.status === 'complete') {
+      chrome.tabs.executeScript(tabId, {
+        backgroundWindow.
+      })
+  }
+});
+
+
+// chrome.extension.onRequest.addListener(
+//     function doStuff(request){
+//         chrome.extension.onRequest.removeListener(doStuff);
+//         other_function(request);
+//     }
+// );
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+    if (changeInfo.status === 'complete') {
+        chrome.tabs.executeScript(tabId, {
+            code: ' if (document.body.innerText.indexOf("Cat") !=-1) {' +
+                  '     alert("Cat not found!");' +
+                  ' }'
+        });
+    }
 });
 
 chrome.runtime.onMessage.addListener(function callback)
 
 var version = "1.0";
 
-//var backgroundWindow = chrome.windows.create(
+// var backgroundWindow = chrome.windows.create(
 //    {url: "background.html?" + tabId, type: "popup", width: 800, height: 600});
 
-function onAttach(tabId) {
-  if (chrome.runtime.lastError) {
-    alert(chrome.runtime.lastError.message);
-    alert(backgroundWindow);
-    return;
-  }
+// function onAttach(tabId) {
+//   if (chrome.runtime.lastError) {
+//     alert(chrome.runtime.lastError.message);
+//     alert(backgroundWindow);
+//     return;
+//   }
 
 
 chrome.browserAction.setIcon({path: 'alertButton.png'});
