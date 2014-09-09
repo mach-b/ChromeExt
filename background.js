c721852b783dc6e9dd319225c3d275dee7e2716d
mach-b <markburton@Marks-MacBook-Pro.local>
2014-09-10 09:43:22 +1200

    update

--- background.js
+++ background.js
@@ -5,12 +5,39 @@
 
 // God, don't use with a default popup
 chrome.browserAction.onClicked.addListener(function(tab){
-    console.log('I am clicked');
-    chrome.browserAction.setIcon({path: 'alertButton.png'});
-    alert("CLICKED!");
+    //console.log('I am clicked');
+    //chrome.browserAction.setIcon({path: 'alertButton.png'});
+    //alert("CLICKED!");
     chrome.tabs.executeScript(null,{file:"contentScript.js"});
 
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  //alert("TAB Updated!"+changeInfo.url);
+  //console.log("tabID = "+tabID+"\nchangeInfo = "+changeInfo)
+});
+
+// On the receiving end, you need to set up an runtime.onMessage event listener
+// to handle the message. This looks the same from a content script or extension page.
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+});
+
+
+// Sending a request from the extension to a content script looks very similar,
+// except that you need to specify which tab to send it to.
+// This example demonstrates sending a message to the content script in the selected tab.
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    console.log(response.farewell);
+  });
+});
+
 
 // var backgroundWindow = chrome.windows.create(
 //    {url: "background.html?" + tabId, type: "popup", width: 800, height: 600});
--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,22 @@
 
 
 //   chrome.extension.sendRequest("message", function(response_str), {alert("Content:"+response_str);});
-alert("CONTENT SCRIPT!");
+//alert("CONTENT SCRIPT!");
 
-console.log('contentScript.js called');
+//console.log('contentScript.js called');
+
+//Sending a request from a content script looks like this:
+chrome.runtime.sendMessage({"greeting": "hello"}, function(response) {
+  console.log(response.farewell);
+});
+
+// On the receiving end, you need to set up an runtime.onMessage event listener
+// to handle the message. This looks the same from a content script or extension page.
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+});
