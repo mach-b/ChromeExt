6813c12250ae81fef84c843b8e38f2f33b6790e2
mach-b <markburton@Marks-MacBook-Pro.local>
2014-09-11 18:56:20 +1200

    update

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 
+var interfaceTabID;
+var interfaceWindowID;
+
+
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   //alert("TAB Updated!"+changeInfo.url);
@@ -34,6 +38,26 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 /////////// OPEN EXTENSION TAB //////////////////////////
   chrome.browserAction.onClicked.addListener(function(activeTab)
 {
-    var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
-    chrome.tabs.create({ url: newURL });
+    var url = 
+    if(interfaceTabID==undefined || interfaceWindowID==undefined) {
+      var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
+      chrome.tabs.create({ url: newURL });
+    }else {
+      // chrome.tabs.update(tabId, {selected: true});
+      //alert("defined")
+      chrome.windows.update(interfaceWindowID, {focused: true});
+      chrome.tabs.update(interfaceTabID, {selected: true});
+
+    }
+
+    // chrome.tabs.query({ active: true, windowId: chrome.windows.WINDOW_ID_CURRENT }, function (tabs) {
+    // // Do something with tabs
+    // });
+    //alert("tabID:"+interfaceTabID+"\nwindowID:"+interfaceWindowID);
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+      interfaceTabID = tabs[0].id;
+      interfaceWindowID = tabs[0].windowId;
+      alert("tabID:"+interfaceTabID+"\nwindowID:"+interfaceWindowID);
+    });
+
 });
